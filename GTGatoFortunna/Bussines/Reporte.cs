56b3072581bbbf8279a446bc81e9c5fbf9126003
Jose Guadalupe Rodriguez Flores <j053_pepe@outlook.com>
2022-01-19 12:48:00 -0600

    Nuevo controlador para pantalla de reportes

---
+++ GTGatoFortunna/Bussines/Reporte.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTGatoFortunna.Bussines
+{
+    public class Reporte
+    {
+        public static Data.Result Get()
+        {
+            try
+            {
+                return FileXML.Query();
+            }
+            catch (Exception error)
+            {
+                return Bussines.Result.GetResult(error, true, null);
+            }
+        }
+    }
+}
---
+++ GTGatoFortunna/Controller/ReporteController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTGatoFortunna.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReporteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = Bussines.Reporte.Get();
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+            else { return BadRequest(result); }
+        }
+    }
+}
