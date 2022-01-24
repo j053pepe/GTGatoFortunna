7a6fbdfb02f1de356fc922985cafe942de4a7bec
Jose Guadalupe Rodriguez Flores <j053_pepe@outlook.com>
2022-01-24 13:17:12 -0600

    Se agrego script de consulta en el js y ocultar los detalles

--- GTGatoFortunna/wwwroot/js/Reportes/Consulta.js
+++ GTGatoFortunna/wwwroot/js/Reportes/Consulta.js
@@ -1,7 +1,31 @@
 ﻿$(function () {
     var consultaFn = {
         init() {
+            $('#mainGato').hide();
+            this.Get();
+        },
+        Get() {
+            $('#ulListData').empty();
+            UniversalFn.Api("Cuenta", "Get")
+                .done(function (data) {
 
+                    $(data.Resultado).each(function () {
+                        $('#ulListData').append(' <li class="w3-bar" id="liItem' + this.CuentaId + '">' +
+                            '<button class="btn"><i class="fas fa-dharmachakra fas-2xl"></i> Tiradas</button>' +
+                            '<img src="data:image/png;base64,' + this.ImgProfile + '" class="w3-bar-item w3-circle w3-hide-small" style="width:100px;height: 85px;">' +
+                            '<div class="w3-bar-item">' +
+                            '<span class="w3-large">' + this.Nick + '</span><br>' +
+                            '<span>' + this.Servidor + '</span>' +
+                            '</div>' +
+                            '</li>');
+
+                        $("#liItem" + this.CuentaId).data('item', this);
+                    });
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    UniversalFn.LogError("Error en la consulta:", jqXHR, textStatus, errorThrown);
+                    alertify.error('Error al consultar.');
+                });
         }
     };
 
